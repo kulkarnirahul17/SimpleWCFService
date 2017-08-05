e06452de374be7f5ae0a4317a56bfff9e0bed42d
Rahul Kulkarni <kulkarnirahul.17@hotmail.com>
2017-08-05 12:57:51 -0400

    Created the working versions of Simple service client, host and service

---
+++ SimpleWCFServiceClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleWCFServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServiceReference1.SimpleServiceClient();
+            for (int i = 1; i <= 100; i++)
+            {
+                var thread = new Thread(client.DoWork);
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ SimpleWCFServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWCFServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(SimpleWCFService.SimpleService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
